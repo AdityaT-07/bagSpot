45589d3b14e5b64b64b25b714b43e9d7dd669146
Aditya Thakur <adityathakur@gmail.com>
2025-12-26 01:15:57 +0530

    modelling bag data

---
+++ models/productmodel.js
@@ -0,0 +1,16 @@
+import {Schema,model} from 'mongoose'
+
+const productSchema = Schema({
+        image : String,
+        name : String,
+        price : Number,
+        discount : {
+            type : Number,
+            default : 0
+        },
+        bgColor : String,
+        panelColor : String,
+        textColor : String
+})
+
+module.exports = model('product',productSchema)
