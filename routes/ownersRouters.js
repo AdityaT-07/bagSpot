b2da6bdd619146cad35e83954f1cd486332fbba3
Aditya Thakur <adityathakur6076@gmail.com>
2025-12-27 14:02:01 +0530

    owner creation check

--- routes/ownersRouters.js
+++ routes/ownersRouters.js
@@ -4,7 +4,11 @@ const ownermodel = require('./models/ownerModel.js')
 if(process.env.NODE_ENV ==='development'){
     router.post('/create', async (req,res)=>{
              let owners = await ownermodel.find();
-            
+             if(owners.length >0){
+               return res
+                .status(502)
+                .send("you dont have permission to create a new owner")
+             }
              let{fullName,email,password,picture} = req.body;
              let createdOwner = await ownermodel.create({
                     fullName,
